e9bc2f1077a860c4d29419b6b7e61617335a25f3
Susanne Dewein <susanne.dewein@gmail.com>
2015-07-29 16:10:57 +0200

    try to catch errors

--- translator.rb
+++ translator.rb
@@ -1,5 +1,6 @@
 class Translator
   attr_reader :filename
+
   def initialize(filename)
     @filename = filename
   end
@@ -7,16 +8,25 @@ class Translator
   def translate
     my_file = JSON.parse(File.read(self.filename))
     all_words = []
+
     my_file.each do |k, v|
       if v == ""
-        adr = URI.escape('http://defi.dict.cc/?s=' + k)
-        html = open(adr)
+        begin
+          adr = URI.escape('http://defi.dict.cc/?s=' + k)
+          html = open(adr)#hier error catchen? und zwischenresult speichern
 
-        doc = Nokogiri::HTML(html)
-        doc.css('#maincontent .td7nl').each {|tag| all_words << tag.content }
-      end
+          doc = Nokogiri::HTML(html)
+          doc.css('#maincontent .td7nl').each {|tag| all_words << tag.content }
+        rescue OpenURI::HTTPError => ex
+          response = ex.io
+          response.status
+          response.string
+          next
+        ensure
+          all_words
+        end
+      end 
+      Hash[*all_words]
     end
-    Hash[*all_words]
   end
-
 end
