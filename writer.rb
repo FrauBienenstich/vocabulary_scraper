5924feba4b80540c7a1209d5fa83cd34d375b558
Susanne Dewein <susanne.dewein@gmail.com>
2015-07-30 15:03:39 +0200

    sanitize output further

--- writer.rb
+++ writer.rb
@@ -9,13 +9,22 @@ class Writer
     File.open(filename, "w") { |f| f.write(converted)}
   end
 
+  def sanitize(string)
+    replacements = [[",",""], ["<S>", ""], ["sanonta","<i>Redewendung:</i>"], ["Unverified", "<i>nicht bestätigt</i>"], ["tähdi.", ""], ["asevoi.", ""], ["kauppa oikeust.", ""], ["usk.", ""], ["elok.", ""], ["enol.", ""]]
+    replacements.each do |sub|
+      string.gsub!(sub[0], sub[1])
+    end
+    string
+  end
+
   def make_nice(filename)
     my_file = JSON.parse(File.read(filename))
     words = []
     my_file.each do |k, v|
       words << "<p>#{k} " + "= " + "#{v}</p>" 
     end
-    words = words.join(', ').gsub(",","")
+    string = words.join(', ')
+    words = sanitize(string)
     "<!DOCTYPE html><html><head><meta charset='UTF-8'></head><body>#{words}</body>"
   end
 
